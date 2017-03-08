ebcec94e391fa5c469a970c52261be11675025bc
yogesh patel <yopatel56@gmail.com>
2017-03-07 23:39:45 -0500

    Start up with Form Creation

--- DollarComputers/ProductInfoForm.Designer.cs
+++ DollarComputers/ProductInfoForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DollarComputers
 {
-    partial class Form1
+    partial class ProductInfoForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ProductInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "ProductInfoForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Product Information Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
